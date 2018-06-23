54d9b7fed5fcf04845b908e1589824a9d2f1ae85
friendlymahi <friendlymahi@gmail.com>
2018-06-23 15:41:16 -0500

    Updated code to make use of WebAppContext from Spring Framework
    Updated Webapp POM to include spring-web dependency
    Updated Webapp web.xml to include context parameters and listener for
    client context initialization while server startup happens

--- productweb/src/main/java/com/demo/sample/ui/servlets/CreateProductServlet.java
+++ productweb/src/main/java/com/demo/sample/ui/servlets/CreateProductServlet.java
@@ -1,10 +1,18 @@
 package com.demo.sample.ui.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.demo.sample.bo.ProductBO;
+import com.demo.sample.dto.Product;
 
 /**
  * Servlet implementation class CreateProductServlet
@@ -17,7 +25,21 @@ public class CreateProductServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
+		int id = Integer.parseInt(request.getParameter("id"));
+		String desc = request.getParameter("desc");
+		String price = request.getParameter("price");
+		
+		Product product = new Product();
+		product.setId(id);
+		product.setDescription(desc);
+		product.setPrice(price);
+		
+		WebApplicationContext context = WebApplicationContextUtils.getRequiredWebApplicationContext(this.getServletContext());
+		ProductBO bo = (ProductBO) context.getBean("productbobean");
+		bo.createProduct(product);
+		PrintWriter out = response.getWriter();
+		out.println("Product Created !!");
 	}
 
 }
--- productweb/src/main/java/com/demo/sample/ui/servlets/DisplayProductServlet.java
+++ productweb/src/main/java/com/demo/sample/ui/servlets/DisplayProductServlet.java
@@ -1,10 +1,18 @@
 package com.demo.sample.ui.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.demo.sample.bo.ProductBO;
+import com.demo.sample.dto.Product;
 
 /**
  * Servlet implementation class DisplayProductServlet
@@ -16,8 +24,22 @@ public class DisplayProductServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		WebApplicationContext context = WebApplicationContextUtils.getWebApplicationContext(this.getServletContext());
+		ProductBO bo = (ProductBO) context.getBean("productbobean"); 
+		
+		Product product = bo.findProduct(Integer.parseInt(request.getParameter("id")));
+		
+		PrintWriter out = response.getWriter();
+		
+		if(product != null)
+		{
+			out.println(product.getDescription() +"::" + product.getPrice());
+		}
+		else
+		{
+			out.println("Sorry. No match found !");
+		}
 	}
 
 }
